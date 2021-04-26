4d130ef6e1be45fddab7545532e076264ad0d056
Rokas <rookas.rudzenskas@gmail.com>
2021-04-26 14:52:21 +0300

    Rokas

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,4 +5,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world! 🚀")
+    return HttpResponse("Amazing 🚀!")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s. 🕵️‍♂️" % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s. ☠️"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s. 🌋" % question_id)
