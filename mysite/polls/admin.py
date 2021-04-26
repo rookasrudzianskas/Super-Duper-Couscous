b3be1eb8e58bf8ac5661cea7ba9f0fdc3f008f6a
Rokas <rookas.rudzenskas@gmail.com>
2021-04-26 14:30:01 +0300

    Rokas

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Question, Questions
+from .models import Question, Choice
 # Register your models here.
+
+admin.site.register(Question, Choice)
+
 
 
